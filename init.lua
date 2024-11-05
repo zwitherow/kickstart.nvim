f272d2cc1397a7eb09504a3eb96d7c602954c4f2
Zach Witherow <zwitherow@gmail.com>
2024-11-04 20:01:15 -0600

    hi

--- init.lua
+++ init.lua
@@ -789,7 +789,7 @@ require('lazy').setup({
     priority = 1000,
     init = function()
       vim.cmd.colorscheme 'catppuccin-mocha'
-      -- vim.cmd.hi 'Comment gui=none'
+      vim.cmd.hi 'Comment gui=italic'
     end,
   },
 
